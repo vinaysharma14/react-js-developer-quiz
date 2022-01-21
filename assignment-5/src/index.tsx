21826d702b9c4aab654b47a55c68ae510448dbcd
Vinay Sharma <vinay.sharma@go-mmt.com>
2022-01-22 02:14:20 +0530

    fixed assignment 5 crash

--- assignment-5/src/index.tsx
+++ assignment-5/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { App } from 'App';
+import { App } from 'app';
 import { store } from 'store';
 
 import './index.scss';
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
